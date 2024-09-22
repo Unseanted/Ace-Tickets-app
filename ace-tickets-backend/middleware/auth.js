171806d5f2c394757c3f337295bb514cf4eb3ed5
unseanted <sinbad4u007@yahoo.com>
2024-09-22 13:01:29 +0100

    vercel json

--- ace-tickets-backend/middleware/auth.js
+++ ace-tickets-backend/middleware/auth.js
@@ -1,7 +1,8 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('./models/User'); // User model
+const User = require('../models/User'); // User model
+
 require('dotenv').config(); // For using environment variables
 
 // Passport JWT options
